c4cf06e2956b66d5a61be650c465d9350149c448
David Souza <daviddavi1221@gmail.com>
2024-11-19 19:41:46 -0300

    subir os arquivos do meu site

---
+++ Portfólio/reveal.js
@@ -0,0 +1,107 @@
+
+  window.revelar = ScrollReveal({reset: true});
+
+  revelar.reveal('.efeito-txt-topo', {
+    duration: 2000,
+    distance: '90px'
+  });
+
+  revelar.reveal('.efeito-img-topo', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+
+  });
+
+  //titulos 
+
+  revelar.reveal('.titulo', {
+    duration: 2000,
+    distance: '90px'
+  });
+
+  //especialidades
+
+  revelar.reveal('.efeito-especialidades1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+  });
+  revelar.reveal('.efeito-especialidades2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+  });
+  revelar.reveal('.efeito-especialidades3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+  });
+  revelar.reveal('.efeito-especialidades4', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2000
+  });
+
+  //fundador
+
+  revelar.reveal('.img-sobre', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+  });
+
+  revelar.reveal('.txt-sobre', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'rigth'
+  });
+
+  //projetos
+
+  revelar.reveal('.efeito-projeto1', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay:500,
+
+
+    
+  });
+  revelar.reveal('.efeito-projeto2', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay:600
+  });
+  revelar.reveal('.efeito-projeto3', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay:700
+  });
+  revelar.reveal('.efeito-projeto4', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay:800
+  });
+  revelar.reveal('.efeito-projeto5', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay:900
+  });
+  revelar.reveal('.efeito-projeto6', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay:1000
+  });
+
+  //formulario
+  revelar.reveal('.efeito-formulario',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'bottom'
+  })
